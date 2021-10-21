f01ff42ea815cba7e18381053569d096fa0722b4
Javier Feliu <javier@wanderin.dev>
2021-10-20 20:29:07 -0500

    feat: Add dayjs and script to modify the way dates are displayed in FAQ

--- src/js/user.js
+++ src/js/user.js
@@ -1,4 +1,23 @@
-// 
-// user.js
-// Use this to write your custom JS
-//
+(function(app) {
+    app.ready = (callback) => {
+        if (document.readyState === "loading") {
+            document.addEventListener("DOMContentLoaded", callback);
+        } else {
+            callback();
+        }
+    }
+
+    app.ready(() => {
+        const relativeTime = window.dayjs_plugin_relativeTime;
+        dayjs.extend(relativeTime);
+
+        // Reformat date for FAQ last update
+        app.reformatDates = () => {
+            document.querySelectorAll(".date-updated").forEach((elem) => {
+                elem.textContent = dayjs(elem.textContent, "MM/DD/YYY").fromNow();
+            });
+        }
+        app.reformatDates();
+
+    });
+})(window.app = window.app || {});
